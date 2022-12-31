09b3569e2e42f4c335a64c4fff947b4e1bab2d9f
José Martins <118491330+jmartinsss@users.noreply.github.com>
2022-12-31 13:38:03 -0300

    CRIANDO DESAFIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("The current time is " + DateTime.Now);
+﻿class Program 
+{
+    static void Main(string[] args)
+    {
+    int a = 4;
+    int b = 8;
+    int c = 0;
+    c = a + b/b - a;
+if(c < 0)
+    c = c + 2;
+else
+    c = c - 1;
+    Console.WriteLine(c);
+    }
+}
+
